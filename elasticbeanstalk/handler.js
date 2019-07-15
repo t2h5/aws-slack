743b4a89fcd4a673c43c0afac7f3ce363a1c2153
tatsuya <t2h5k0@gmail.com>
2019-07-15 21:36:34 +0900

    Update packages for elasticbeanstalk

--- elasticbeanstalk/handler.js
+++ elasticbeanstalk/handler.js
@@ -11,7 +11,7 @@ let hookUrl
 
 function postMessage (message, callback) {
   const body = JSON.stringify(message)
-  const options = url.parse(hookUrl)
+  const options = url.URL(hookUrl)
   options.method = 'POST'
   options.headers = {
     'Content-Type': 'application/json',
